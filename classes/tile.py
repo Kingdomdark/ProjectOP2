8a3cdb2702e2addb1b321824b5b125babf1bc313
Uxilo <jmolendijk93@gmail.com>
2017-01-18 12:24:22 +0100

    Changed some base funcs of the manager and tile.

--- classes/tile.py
+++ classes/tile.py
@@ -1,11 +1,12 @@
 from vector2 import vector2
-from pygame
+import pygame
 
 class tile:
-    def __init__(self, posx,posy,width,height):
+    def __init__(self,posx,posy,width,height,category):
         self.position = vector2(posx,posy)
         self.size = vector2(width,height)
         self.tiledirections = None
+        self.category = category
 
     # Pass in the tiles for the directions.
     def setupDirections(self,up,right,down,left):
@@ -13,6 +14,9 @@ class tile:
 
     # Add drawing functionality here using Pygame.
     def draw(self):
+        pass
+
+    def update(self):
         pass
 
 # Used to store the adj. tiles.
