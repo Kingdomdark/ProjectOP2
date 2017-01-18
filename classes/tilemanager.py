323800279393ce24a0aa838f3d48dc54c8f8f00c
Uxilo <jmolendijk@gmail.com>
2017-01-18 12:24:22 +0100

    Changed some base funcs of the manager and tile.

--- classes/tilemanager.py
+++ classes/tilemanager.py
@@ -2,22 +2,42 @@ import tile
 
 class tilemanager:
     def __init__(self):
-        self.tiles = [] # Tile Array to store the tiles in.
+        self.size = vector2(25,25)
+        self.sides = []
 
-#------------------------------------------------------------------
+#--------------------------------------------------------------------------
+# Adding sides
+#--------------------------------------------------------------------------
+        self.sides.append(side(0,0))
+
+        #shortcodes
+        tl1 = self.sides[0].tiles
+#--------------------------------------------------------------------------
 # Adding tiles
-#------------------------------------------------------------------
+#--------------------------------------------------------------------------
         # First row [0 t/m ...]
-        self.tiles.append(tile(0,0,50,50))
-        self.tiles.append(tile(0,0,50,50))
-#------------------------------------------------------------------
-# Linking the tiles
-#------------------------------------------------------------------
-        # First row [0 t/m ...]
-        self.tiles[0].setupDirections(None, None, None, None)
-        self.tiles[1].setupDirections(self.tiles[0],None,None,None)
+        self.sides[0].tiles.append(tile(0,0,self.size.x,self.size.y))
+        self.sides[0].tiles.append(tile(0,0,self.size.x,self.size.y))
 
+#--------------------------------------------------------------------------
+# Linking the tiles
+#--------------------------------------------------------------------------
+        # First row [0 t/m ...]
+        self.sides[0].tiles[0].setupDirections(None, None, None, None)
+        self.sides[0].tiles[1].setupDirections(self.sides[0].tiles[0],None,None,None)
+#--------------------------------------------------------------------------
+# Invoking functionality
+#--------------------------------------------------------------------------
     def draw(self):
         # For every tile in tiles, draw the tile.
         for t in tiles:
             t.draw()
+
+    def update(self):
+        # For every tile in tiles, update the tile.
+        for t in tiles:
+            t.update()
+
+class side:
+    def __init__(self,boardimage,x,y):
+        self.tiles = []
