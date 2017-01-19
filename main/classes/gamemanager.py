31cd20ad2ee0f3540413c949b27c8819d3a1546a
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-01-19 14:06:52 +0100

    Geen idee hoe je van scherm wisseld

---
+++ main/classes/gamemanager.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class gamemanager:
+
+    def loadgame(self ):
+        pass
--- main/classes/menumanager.py
+++ main/classes/menumanager.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+from classes.gamemanager import  gamemanager
 class menumanager:
     def __init__(self ,screen):
         self.screen = screen
@@ -11,35 +11,39 @@ class menumanager:
         bt_start = pygame.image.load("images/start-bt.png")
         bt_high  = pygame.image.load("images/highscore-bt.png")
         bt_exit  = pygame.image.load("images/exit-bt.jpg")
+        bt_inst  = pygame.image.load("images/instr-bt.png")
 
         #print on screen
-        start = self.screen.blit(bt_start, (350 ,100))
-        high  = self.screen.blit(bt_high, (310, 250))
-        exit  = self.screen.blit(bt_exit, (350, 470))
+        start = self.screen.blit(bt_start, (350 ,0))
+        high  = self.screen.blit(bt_high, (310, 80))
+        instr = self.screen.blit(bt_inst, (350, 300))
+        exit  = self.screen.blit(bt_exit, (350, 500))
+
 
         # add button handlers
-        self.buttonClick(start,high,exit)
+        self.buttonClick(start,high,instr , exit)
 
-    def buttonClick(self, start,high,exit):
-
+    def buttonClick(self, start,high,instr , exit):
+            gm = gamemanager()
+            page = ""
             #mouse buttons
             for e in pygame.event.get():
                 if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
                     pos = pygame.mouse.get_pos()
                     # start button is pressed > Open Game
                     if start.collidepoint(pos):
-                        print ("Start button is pressed")
+                         page = "start"
                     # Highscore button is pressed > Open Highscore
                     elif high.collidepoint(pos):
-                        print("highscore button is pressed")
-                    # Exit button is pressed > Open Game
+                        page = "highscore"
+                    elif instr.collidepoint(pos):
+                        page = "instructions"
+                    # Exit button is pressed > Exit game
                     elif exit.collidepoint(pos):
                            pygame.quit();
                            sys.exit();
-                    # exit game
-                    elif e.type == pygame.QUIT:
-                            pygame.quit();
-                            sys.exit();
+                    print (page)
+
 
 
 
