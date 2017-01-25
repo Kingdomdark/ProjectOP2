5c187b307d29752217042f2d51cfc89567c01f20
Uxilo <jmolendijk93@gmail.com>
2017-01-25 10:11:49 +0100

    Part 2 of the changes

--- main/classes/AudioVisualManager.py
+++ main/classes/AudioVisualManager.py
@@ -1,12 +1,20 @@
 import os
 import pygame
 
+# Spritemanager (Dictionary) which contains the sounds which can be used and has functions to return images
+# which in return can be used to drawn on the screen.
 class spritemanager:
     def __init__(self):
         self.imgfolder = os.path.join(os.getcwd(),"images")
         self.sprites = {
             "background" : os.path.join(self.imgfolder,"background.png"),
             "logo" : os.path.join(self.imgfolder,"logo.png"),
+            "start" : os.path.join(self.imgfolder,"start.png"),
+            "start_hover" : os.path.join(self.imgfolder,"start_hover.png"),
+            "instructions" : os.path.join(self.imgfolder,"instructions.png"),
+            "instructions_hover" : os.path.join(self.imgfolder,"instructions_hover.png"),
+            "quitgame" : os.path.join(self.imgfolder,"quitgame.png"),
+            "quitgame_hover" : os.path.join(self.imgfolder,"quitgame_hover.png"),
             "tile" : os.path.join(self.imgfolder,"tile.png")
         }
 
@@ -16,18 +24,30 @@ class spritemanager:
         except:
             print("AVManager : An error occured whilst importing the image '%s', It might not exist" % imgname)
 
+# Audiomanager (Dictionary) which contains the sounds which can be used and has functions to play specific sounds on
+# the specified volume.
 class audiomanager:
     def __init__(self):
         self.soundfolder = os.path.join(os.getcwd(),"sounds")
         self.sounds = {
-            "wakemeup" : os.path.join(self.soundfolder,"wakemeup.mp3")
+            "menumusic" : os.path.join(self.soundfolder,"menu-music.mp3"),
+            "menubutton" : os.path.join(self.soundfolder, "menu-button.ogg")
         }
 
-    def playmusicloop(self,soundname):
+    def playmusicloop(self,soundname,volume):
         try:
             pygame.mixer.music.load(self.sounds[soundname.lower()])
-            pygame.mixer.music.set_volume(.05)
+            pygame.mixer.music.set_volume(volume)
             pygame.mixer.music.play(-1)
+            print("AVManager : Playing %s" % soundname)
+        except:
+            print("AVManager : An error occured whilst trying to play the sound '%s', It might not exist." % soundname)
+
+    def playsound(self,soundname,volume):
+        try:
+            s = pygame.mixer.Sound(self.sounds[soundname.lower()])
+            s.set_volume(volume)
+            s.play(0)
             print("AVManager : Playing %s" % soundname)
         except:
             print("AVManager : An error occured whilst trying to play the sound '%s', It might not exist." % soundname)
--- main/main.py
+++ main/main.py
@@ -1,37 +1,45 @@
+from classes.MenuManager import menumanager
+from classes.ContentManager import contentmanager
 import math
 import pygame
-from  classes.menumanager import menumanager
+import sys
 
-class Game:
+class Main:
     def __init__(self):
         # Resolution
-        width = 1000
-        height = 700
-        size = (width,height)
+        self.width = 1000
+        self.height = 700
+        self.size = (self.width,self.height)
+
         # Start Pygame
         pygame.init()
 
         # Set the Resolution
-        self.screen = pygame.display.set_mode(size)
+        self.screen = pygame.display.set_mode(self.size)
 
         # managers
-        self.mm = menumanager(self.screen)
+        self.cm = contentmanager(self.screen)
+
+        # Mouse position
+        self.pos = pygame.mouse.get_pos()
 
         # Set window title
-        pygame.display.set_caption("ProjectOP2","ProjectOP2")
+        pygame.display.set_caption("Euromasterdam","ProjectOP2")
 
     # Update everything
     # NOTE / TODO : To keep things tidy, create managers for handling
     def update(self):
-        pass
+        self.cm.update()
+
 
     # Draw everything
     # NOTE / TODO : To keep things tidy, create managers for handling
     def draw(self):
         #Clear the screen
         self.screen.fill((0 , 0 , 0 ))
-        self.mm.drawbuttons(self)
+
         #---- BEGIN DRAW LOGIC ----
+        self.cm.draw()
 
 
         #----- END DRAW LOGIC -----
@@ -44,22 +52,37 @@ class Game:
         # but it's currently allowed since the only event at the moment
         # is the 'Quit' events. Should we manage multiple events, we
         # need to fix process_events. - Jordan
-        while not process_events():
+        while True:
+            self.process_events()
             self.update()
             self.draw()
 
 
-# Handle pygame events
-def process_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            # Give the signal to quit the game.
-            return True
-    return False
+    # Handle pygame events
+    def process_events(self):
+        self.pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            # Quit game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            # Mouse hover handling
+            if self.cm.stage == 0:
+                self.cm.mm.bhoverhandling(self.pos)
+            # Mouse button down handling
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                #If on menu, check for menu events @ Buttons.
+                #----------- MENU MANAGER ------------
+                if self.cm.stage == 0:
+                    self.cm.mm.bclickhandling(self.pos)
+
+
+
+
 
 # Main program logic
 def program():
-    game = Game()
+    game = Main()
     game.gameloop()
 
 program()
